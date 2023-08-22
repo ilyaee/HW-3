b145bcf3cb0681761c14b48df57c0dca590be084
Ilya <elisov97@gmail.com>
2023-08-22 08:10:38 +0300

    dev

--- task2.js
+++ task2.js
@@ -7,7 +7,22 @@ let city = prompt("Введите город: ")
 
 const url = `http://api.weatherstack.com/current?access_key=${myAPIKey}&query=${city}`
 
+
 http.get(url, (res) => {
+    const {statusCode} = res
+    if (statusCode !== 200) {
+        console.log(`statusCode: ${statusCode}`)
+        return
+    }
 
+    res.setEncoding('utf8')
+
+    let rowData = ''
+    res.on('data', (chunk) => rowData += chunk)
+    res.on('end', () => {
+        let parseData = JSON.parse(rowData)
+        console.log(parseData)
+    })
+}).on('error', (err) => {
+    console.error(err)
 })
-
